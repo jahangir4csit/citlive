ee2d5ab8651432182ed7998fdab12d3442812b5b
Jahangir Ahmed <33389473+jahangir4csit@users.noreply.github.com>
2022-04-27 13:56:30 +0600

    Feedback iframe height

--- src/components/homepage/feedbackSliderOne.js
+++ src/components/homepage/feedbackSliderOne.js
@@ -65,7 +65,7 @@ export default function FeedbackSliderOne(data){
                     <div>
                         <div class="feedback_item">
                             <iframe src={`https://www.facebook.com/plugins/post.php?href=${review.crb_fb_review_url}&show_text=true&appId=191181859164389`} 
-                             style={{border:'none', overflow:'hidden', width: '100%', height: 'auto'}} 
+                             style={{border:'none', overflow:'hidden', width: '100%', height: '100vh', paddingRight: '1px'}} 
                             scrolling="no" frameborder="0" allowfullscreen="true" 
                             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
                             </iframe>
