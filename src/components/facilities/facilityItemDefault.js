9af8fffda5d9b88ae9e36e4f951be34d9f0aad4e
Jahangir Ahmed <33389473+jahangir4csit@users.noreply.github.com>
2022-04-27 15:56:35 +0600

    update thumb condition

--- src/components/facilities/facilityItemDefault.js
+++ src/components/facilities/facilityItemDefault.js
@@ -16,6 +16,7 @@ export default function FacilityItemDefault(data){
             <div class="intarnship support_item" style={{ backgroundColor: facilityDetails.facility_sec_bg}}>
                 <h3 dangerouslySetInnerHTML={{ __html: facilityDetails.facility_sec_title }} />
                 <p dangerouslySetInnerHTML={{ __html: facilityDetails.facility_sec_description }} />
+                {facilityDetails.facility_sec_video_id &&
                 <div class="support_item_img">
                     <img class="img-fluid w-100" src={facilityDetails.facility_sec_video_thumb} alt="image" />
                     <div class="overly_icon">
@@ -25,6 +26,7 @@ export default function FacilityItemDefault(data){
                         </button>
                     </div>
                 </div>
+                }
             </div>
         </div>
     )
