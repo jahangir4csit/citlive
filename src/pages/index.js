347dd2a3c2ba9656a9ba53728ac6e3d9368329f8
unknown <jahangir4csit@gmail.com>
2022-05-23 11:11:32 +0600

    motion and lazyload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { graphql } from "gatsby"
 import * as React from "react"
+import LazyLoad from 'react-lazyload';
 import BlogSlider from "../components/blog/blogSlider"
 import CounterUp from "../components/counterUp"
 import AdmissionCourses from "../components/courses/admissionCourses"
@@ -30,13 +31,20 @@ export default function HomePage({data}){
 
   return(
     <Layout>
-      <Seo title="Home" />
-      <HomeBanner />
-      <DepartmentSlider courseCatInfo={courseData} />
+      <Seo 
+      title="Professional IT Training Institute in Bangladesh - Creative IT"
+      description="Develop your professional personal skills through Creative IT Institute We provide
+    Graphic Design, Web Software, Digital Marketing, Networking, Film Media,
+    Robotics Automation Training etc affordable price. Call +880 162 4666000"
+       />
+      <HomeBanner/>
+      <DepartmentSlider 
+      courseCatInfo={courseData} 
+      />
       <CourseSlide 
       secHeading={homeData.homeFavouriteCourseSecHeading} 
       secDesc={homeData.homeFavouriteCourseSecDesc} 
-      coursSlide={courseData} 
+      coursSlide={courseData}
       />
       <CitInfoSlider />
       <StudentSuccess 
@@ -54,7 +62,9 @@ export default function HomePage({data}){
       secDesc={homeData.homeCommentsSecDesc}
       data={reviewsData}
       />
-      <VirtualTour vData={vitrualTour} />
+      <LazyLoad once>
+        <VirtualTour vData={vitrualTour} />
+      </LazyLoad>
       <AdmissionCourses 
       secHeading={homeData.homeAdmissionSecHeading} 
       secDesc={homeData.homeAdmissionSecDesc}
@@ -75,10 +85,10 @@ export default function HomePage({data}){
       secDesc={homeData.homePartnersSecDesc}
       partnersData={homeData.citPartners}
       />
-      <BlogSlider 
+      {/* <BlogSlider 
       secHeading={homeData.homeBlogSecHeading} 
       secDesc={homeData.homeBlogSecDesc}
-      />
+      /> */}
     </Layout>
   )
 }
