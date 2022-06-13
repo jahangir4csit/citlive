b284e601d8003b9a6d52e3645373e8ac0c924f02
Jahangir Ahmed <33389473+jahangir4csit@users.noreply.github.com>
2022-06-13 08:32:08 +0600

    config page 100

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
         //url: `http://localhost/citbd/graphql`,
         schema: {
           timeout: 350000,
-          perPage: 50,
+          perPage: 100,
           requestConcurrency: 15
         }
       },
